b9a353e4d97276ca536b59fd1bef4aaa293606bc
atefehmk <102993614+atefehmk@users.noreply.github.com>
2023-10-14 23:01:29 +0330

    Update init.c

--- xv6-public-master/init.c
+++ xv6-public-master/init.c
@@ -21,6 +21,10 @@ main(void)
 
   for(;;){
     printf(1, "init: starting sh\n");
+    printf(1,"Group #29:\n");
+    printf(1,"Elaheh Khodaverdi\n");
+    printf(1,"Fereshteh Bagheri\n");
+    printf(1,"Atefeh Mirzakhani\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
