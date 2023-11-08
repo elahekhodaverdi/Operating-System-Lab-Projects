49336d97248134373dc2539e8ed239f6227e8068
atefehmk <102993614+atefehmk@users.noreply.github.com>
2023-11-08 15:30:21 +0330

    add find_digital_root function

--- xv6-public-master/proc.c
+++ xv6-public-master/proc.c
@@ -532,3 +532,16 @@ procdump(void)
     cprintf("\n");
   }
 }
+
+int 
+find_digital_root(int n) {
+    while (n >= 10) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        n = sum;
+    }
+    return n;
+}
