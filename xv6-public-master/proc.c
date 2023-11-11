cc2694c2a41e97b46c157da386198b29eb58e283
Fereshte Bagheri <102649649+FereshteBagheri@users.noreply.github.com>
2023-11-11 16:30:37 +0330

    add find_digital_root to proc.c back

--- xv6-public-master/proc.c
+++ xv6-public-master/proc.c
@@ -533,6 +533,19 @@ procdump(void)
   }
 }
 
+int 
+find_digital_root(int n) {
+    while (n >= 10) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        n = sum;
+    }
+    return n;
+}
+
 int get_child_count(void){
   struct proc* curr = myproc();
   struct proc* grand_parent = curr->parent->parent;
